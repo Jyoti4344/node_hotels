49343dd309e99d3dddb346a25a46e4b51e249697
Jyoti4344 <jyotigoyal4344@gmail.com>
2024-09-21 18:49:48 +0530

    mongo db online shift

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Welcome to my hotel.. how can I help you?');
@@ -15,6 +17,6 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
   console.log("listening to port 3000");
 })
